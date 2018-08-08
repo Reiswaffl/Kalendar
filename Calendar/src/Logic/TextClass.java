52a2504b9be5d2181d6cc406cd7c96874466c4b9
Reiswaffl <38401280+Reiswaffl@users.noreply.github.com>
2018-08-08 20:40:07 +0200

    Comitten

--- Calendar/src/Logic/TextClass.java
+++ Calendar/src/Logic/TextClass.java
@@ -7,7 +7,7 @@ import XML.Writer;
 import javax.xml.transform.TransformerException;
 
 public class TextClass{
-    public static void Logic(String[] args) throws TransformerException {
+    public static void main(String[] args) throws TransformerException {
         String a = "16:30";
         String b;
         b = a.substring(0,a.length()-3);
