cc4463de7f7749e45970f09de21acf9068c5e3ea
Reiswaffl <38401280+Reiswaffl@users.noreply.github.com>
2018-10-31 20:54:40 +0400

    fancy

--- Calendar/src/GUI/DayReviewSceneController.java
+++ Calendar/src/GUI/DayReviewSceneController.java
@@ -8,8 +8,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -17,17 +19,35 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class DayReviewSceneControlle implements Initializable{
+public class DayReviewSceneController implements Initializable{
     Logic Logic = new Logic();
     @FXML
     Text time;
-    @FXML Text information;
-    @FXML TextField dayinput;
+    @FXML private Text information;
+    @FXML private TextField dayinput;
+    @FXML private Label head;
+    @FXML private Button back;
+    @FXML private Button select;
+    @FXML private Label Time;
+    @FXML private Label Information;
+    @FXML private Label dayinputhead;
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        head.setTextFill(Color.rgb(0,102,200));
+        Time.setTextFill(Color.rgb(0,102,200));
+        dayinputhead.setTextFill(Color.rgb(0,102,200));
+        Information.setTextFill(Color.rgb(0,102,200));
+        back.setStyle("-fx-background-color: #0066C8;");
+        back.setTextFill(Color.rgb(255,255,255));
+        select.setStyle("-fx-background-color: #0066C8;");
+        select.setTextFill(Color.rgb(255,255,255));
         try {
+
             information.setText(Logic.getDayInfo(0, "null", false));
             time.setText(Logic.getDayTimes(0, "null", false));
+
         }catch (Exception e){}
     }
     public void back(ActionEvent event) throws IOException {
@@ -46,7 +66,7 @@ public class DayReviewSceneControlle implements Initializable{
             alert.setTitle("Fehler beim Eintragen");
 
             //alert.setHeaderText("Es ist leider ein Fehler aufgetreten");
-            alert.setContentText("Fehler bei der Eingabe, bitte versuchen sie es nocheinmal nach der beschriebene Schreibweise");
+            alert.setContentText("Fehler bei der Eingabe, bitte versuchen sie es nocheinmal nach der beschriebenen Schreibweise");
             alert.showAndWait();
         }else {
             try {
