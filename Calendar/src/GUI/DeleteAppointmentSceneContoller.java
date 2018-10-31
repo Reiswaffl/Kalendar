393e52b58dfa2bb62c4506e7ae63a2c04e5a1513
Reiswaffl <38401280+Reiswaffl@users.noreply.github.com>
2018-10-31 18:47:47 +0400

    zaunig

--- Calendar/src/GUI/DeleteAppointmentSceneContoller.java
+++ Calendar/src/GUI/DeleteAppointmentSceneContoller.java
@@ -2,14 +2,13 @@ package GUI;
 
 import Logic.Logic;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Alert;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -23,8 +22,37 @@ public class DeleteAppointmentSceneContoller implements Initializable{
     @FXML private TextField start;
     @FXML private  TextField input;
     @FXML private MenuButton repetition;
-
+    @FXML private Label head;
+    @FXML private Label normal;
+    @FXML private Label required;
+    @FXML private Label dayinputhead;
+    @FXML private Label inputhead;
+    @FXML private Label perm;
+    @FXML private Label starthead;
+    @FXML private Label repetitionhead;
+    @FXML private Button back;
+    @FXML private Button confirm;
     Repetition rep;
+
+    public void initialize(URL location, ResourceBundle resources) {
+        repetition.setText("wöchentlich");
+        rep = Repetition.WEEKLY;
+        inputhead.setTextFill(Color.rgb(0,102,200));
+        head.setTextFill(Color.rgb(0,102,200));
+        normal.setTextFill(Color.rgb(0,102,200));
+        dayinputhead.setTextFill(Color.rgb(0,102,200));
+        perm.setTextFill(Color.rgb(0,102,200));
+        repetitionhead.setTextFill(Color.rgb(0,102,200));
+        required.setTextFill(Color.rgb(0,102,200));
+        starthead.setTextFill(Color.rgb(0,102,200));
+        back.setTextFill(Color.rgb(255,255,255));
+        back.setStyle("-fx-background-color: #0066C8;");
+        confirm.setStyle("-fx-background-color: #0066C8;");
+        confirm.setTextFill(Color.rgb(255,255,255));
+        repetition.setStyle("-fx-background-color: #0066C8;");
+        repetition.setTextFill(Color.rgb(255,255,255));
+    }
+
     @FXML
     public void handleWeekly(){
         repetition.setText("wöchentlich");
@@ -46,11 +74,6 @@ public class DeleteAppointmentSceneContoller implements Initializable{
         rep = Repetition.YEARLY;
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        repetition.setText("wöchentlich");
-        rep = Repetition.WEEKLY;
-    }
     @FXML
     public void back(ActionEvent event) throws IOException {
         Parent addAppiontment = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
